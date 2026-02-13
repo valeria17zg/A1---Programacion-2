307c77bcc32850f6f8cee47882026a9643cdbc74
JulianLadino18 <118938730+JulianLadino18@users.noreply.github.com>
2026-02-12 20:38:34 -0500

    Nueva linea de print

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     System.out.println("Hola Mundo");
+    System.out.println("Print creado por Julian");
+
 }
