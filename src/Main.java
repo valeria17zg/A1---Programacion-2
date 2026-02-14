f82a3d9b1f8e05d030693c50cfad4faaad95ca85
JulianLadino18 <118938730+JulianLadino18@users.noreply.github.com>
2026-02-14 18:55:13 -0500

    Print para ejecutar git pull

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ void main() {
     System.out.println("Hola Mundo");
     System.out.println("Print creado por Julian");
     System.out.println("Print descargado con 'git pull'");
+    System.out.println("Print descargado con 'git pull'");
 
 }
