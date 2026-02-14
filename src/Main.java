a0b68962da42b46267d3be155313506e17c45aac
Valeria <valeria.zapatag@uqvirtual.edu.co>
2026-02-14 18:59:10 -0500

    segundo cambio

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     System.out.println("Cambio por Valeria");
+    System.out.println("Segundo cambio por Valeria");
+
 }
